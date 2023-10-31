0ab94a4599eb6ec20666b1966619f4f52aea2068
Miguel A. Gómez <>
2023-10-31 22:06:30 +0000

    update main

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ import {Factory} from "./infrastructure/factory";
 
 const server = Factory.createServer();
 const PORT = 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${ PORT }`));
