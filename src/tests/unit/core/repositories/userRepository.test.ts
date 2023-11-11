3ec321f59f1bbafed40ac5387babfc2ce6ffe221
Miguel A. Gómez <>
2023-11-11 12:02:52 +0000

    improve test names

--- src/tests/unit/core/repositories/userRepository.test.ts
+++ src/tests/unit/core/repositories/userRepository.test.ts
@@ -4,32 +4,34 @@ import {Id} from "../../../../core/valueObjects/id";
 import {Password} from "../../../../core/valueObjects/password";
 import {InMemoryUserRepository, UserRepository} from "../../../../core/repositories/userRepository";
 
-describe('The In Memory User Repository', () => {
-    let repo: UserRepository;
+describe('The In Memory User Repository', ()=>{
+    let repo: InMemoryUserRepository;
 
-    beforeEach(() => {
+    beforeEach(()=>{
         repo = new InMemoryUserRepository();
     });
 
-    it('saves and finds a user by ID', async () => {
+    it('finds a user by ID', async ()=>{
         const id = Id.generateUniqueId();
-        const user = createUser({id});
+        const user = createUserById(id);
         await repo.save(user);
 
-        const retrievedUser = await repo.findById(id);
-        expect(retrievedUser).toEqual(user);
+        const foundUser = await repo.findById(id);
+
+        expect(foundUser).toEqual(user);
     });
 
-    it('does not find a non-existent user by ID', async () => {
+    it('does not find a non-existing user by ID', async ()=>{
         const id = Id.generateUniqueId();
 
-        const retrievedUser = await repo.findById(id);
-        expect(retrievedUser).toBeUndefined();
+        const foundUser = await repo.findById(id);
+
+        expect(foundUser).toBeUndefined();
     });
 
-    it('saves and finds a user by email', async () => {
+    it('finds a user by Email', async ()=>{
         const email = Email.create('test@example.com')
-        const user = createUser({});
+        const user = createUserByEmail(email);
         await repo.save(user);
 
         const foundUser = await repo.findByEmail(email);
@@ -37,44 +39,65 @@ describe('The In Memory User Repository', () => {
         expect(foundUser).toEqual(user);
     });
 
-    it('does not find a user by a non-existent email', async () => {
-        const email = Email.create('nonexistent@example.com');
+    it('does not find a non-existing user by Email', async ()=>{
+        const email = Email.create('test@example.com')
 
         const foundUser = await repo.findByEmail(email);
 
         expect(foundUser).toBeUndefined();
     });
 
-    it('finds all users', async () => {
-        const user1 = createUser({});
-        const user2 = createUser({email:Email.create('test2@example.com')});
-        await repo.save(user1);
-        await repo.save(user2);
+    it('finds all users', async ()=>{
+        const aUser = createUserByEmail(Email.create('test1@example.com'));
+        const anotherUser = createUserByEmail(Email.create('test2@example.com'));
+        await repo.save(aUser);
+        await repo.save(anotherUser);
 
         const users = await repo.findAll();
 
         expect(users).toHaveLength(2);
+        expect(users).toEqual([aUser, anotherUser]);
     });
 
-    it('finds no users when the repository is empty', async () => {
+    it('finds no users when the repository is empty', async ()=>{
         const users = await repo.findAll();
 
         expect(users).toHaveLength(0);
+        expect(users).toEqual([]);
     });
 
-    it('removes an user', async () => {
-        const id = Id.generateUniqueId();
-        const user = createUser({id});
+    it('removes an user', async ()=>{
+        const email = Email.create('test@example.com')
+        const user = createUserByEmail(email);
         await repo.save(user);
 
         await repo.remove(user);
-        const retrievedUser = await repo.findById(id);
+        const foundUser = await repo.findByEmail(email);
 
-        expect(retrievedUser).toBeUndefined();
+        expect(foundUser).toBeUndefined();
     });
+
+    it('updates an user when its already exists', async ()=>{
+        const aUser = createUserByEmail(Email.create('test1@example.com'));
+        const sameUser = aUser;
+        await repo.save(aUser);
+        await repo.save(sameUser);
+
+        const users = await repo.findAll();
+
+        expect(users).toHaveLength(1);
+        expect(users).toEqual([aUser]);
+    })
 });
 
-function createUser({id = Id.generateUniqueId(), email = Email.create('test@example.com') }:{id?: Id, email?: Email}) {
+function createUserById(id: Id) {
+    const password = Password.createFromPlainText('TestPass123_');
+    const email = Email.create('test@example.com')
+    return new User(id, email, password);
+}
+
+function createUserByEmail(email: Email) {
+    const id = Id.generateUniqueId();
     const password = Password.createFromPlainText('TestPass123_');
     return new User(id, email, password);
 }
